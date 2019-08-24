51a02e4127b16648ed172b273fdb4bf77001aed9
KIMINJOONG <ijboym216@gmail.com>
2019-08-24 22:55:53 +0900

    webpack build

---
+++ src/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+//import path from "path";
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ["@babel/polyfill", ENTRY_FILE],
+    mode: MODE,
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: [
+                    {
+                        loader:"babel-loader"
+                    }
+                ]
+            },
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+};
+
+module.exports = config;
