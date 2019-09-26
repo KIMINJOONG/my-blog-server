4c292955abab8c0a8292da6de49f4a6a89f10b76
KIMINJOONG <ijboym216@gmail.com>
2019-09-26 09:20:08 +0900

    change express start

--- src/server.js
+++ src/server.js
@@ -3,10 +3,20 @@ import "./db";
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
 const PORT = process.env.PORT || 3000;
 
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+// const handleListening = () =>
+  // console.log(`Listening on: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+// app.listen(PORT, handleListening);
+const options = {
+  key: fs.readFileSync(process.env.KEY),
+};
+
+http.createServer(app).listen(PORT);
+
+https.createServer(options,app).listen(443);
