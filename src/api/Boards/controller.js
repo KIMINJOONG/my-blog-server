e1e87c1a742afdeff842aa9c6e90b97f35c63eca
KIMINJOONG <ijboym216@gmail.com>
2019-10-11 01:03:25 +0900

    boards controller and service

--- src/api/Boards/controller.js
+++ src/api/Boards/controller.js
@@ -1,39 +1,16 @@
 import Board from "../../models/Board";
+import BoardsService from './service';
 
 export default {
     getBoardList: async(req, res) => {
       const { query } = req;
       const { params: {categoryId} } = req;
-      const page = query.page ? query.page : 1;
-      const pageNum = query.pageNum ? query.pageNum : 10;
-      const start = (page - 1) * pageNum;
-      const limit = pageNum;
-      let totalCount = await Board.find({ category: categoryId });
-      totalCount = totalCount.length;
-      let boards = null;
-      if(query.searchValue === 'undefined') {
-        boards = await Board.find({ category: categoryId }).sort({createdAt : 'desc'}).skip(parseInt(start, 10)).limit(parseInt(limit, 10));
-        const result = {
-          boards,
-          totalCount
-        }
-        return res.status(200).json(result);
-      }
-      if(query.searchValue !== 'undefined') {
-        boards = await Board.find({
-          $and: [{
-            title: { 
-              $regex: '.*' + query.searchValue + '.*' 
-            }
-          },{
-            category: categoryId
-          }]
-        }).sort({createdAt : 'desc'}).skip(parseInt(start, 10)).limit(parseInt(limit, 10));
-        const result = {
-          boards,
-          totalCount
-        }
-        return res.status(200).json(result);
+      
+      try {
+        const response = await BoardsService.getBoardList(query, categoryId);
+        return res.status(200).json(response);
+      }catch(err) {
+        return res.status(500).json(err);
       }
     }
 }
---
+++ src/api/Boards/service.js
@@ -0,0 +1,45 @@
+import Board from "../../models/Board";
+
+export default {
+    getBoardList: async(query, categoryId) => {
+      const page = query.page ? query.page : 1;
+      const pageNum = query.pageNum ? query.pageNum : 10;
+      const start = (page - 1) * pageNum;
+      const limit = pageNum;
+      let totalCount = await Board.find({ category: categoryId });
+      totalCount = totalCount.length;
+      let boards = null;
+      if(query.searchValue === 'undefined') {
+        boards = await Board.find({ category: categoryId }).sort({createdAt : 'desc'}).skip(parseInt(start, 10)).limit(parseInt(limit, 10));
+        return {
+            boards,
+            totalCount
+        }
+      }
+      if(query.searchValue !== 'undefined') {
+        totalCount = await Board.find({
+            $and: [{
+              title: { 
+                $regex: '.*' + query.searchValue + '.*' 
+              }
+            },{
+              category: categoryId
+            }]
+        });
+        totalCount = totalCount.length;
+        boards = await Board.find({
+          $and: [{
+            title: { 
+              $regex: '.*' + query.searchValue + '.*' 
+            }
+          },{
+            category: categoryId
+          }]
+        }).sort({createdAt : 'desc'}).skip(parseInt(start, 10)).limit(parseInt(limit, 10));
+        return {
+            boards,
+            totalCount
+        }
+      }
+    }
+}
