847f8014351f63ede6bd1304c8fe93e861ad1e65
KIMINJOONG <ijboym216@gmail.com>
2019-12-29 18:58:22 +0900

    유저 정보 로그인체크

--- src/api/User/index.js
+++ src/api/User/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import userController from './controller';
+import { isLoggedIn, isNotLoggedIn } from '../../utils/checkLogin';
 
 const router = express.Router();
 router.post('/', userController.join);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/', userController.getUser);
+router.post('/login', isNotLoggedIn, userController.login);
+router.post('/logout', isLoggedIn, userController.logout);
+router.get('/', isLoggedIn, userController.getUser);
 
 export default router;
